4480e3ff67f45c510df128c8994bb7cb6779ab54
lincolneddy <lincoln@essentialcaravans.com>
2025-04-07 12:18:26 +1000

    added

---
+++ esse_extentions/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Essential Product Extensions',
+    'author' : 'Lincoln Eddy',
+    'version' : '1.0',
+    'depends' : ['product'],
+    'data' : [
+        'views/product_template.xml'
+    ]
+}
