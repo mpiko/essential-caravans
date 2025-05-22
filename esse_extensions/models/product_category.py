3306f86d18f8401271c8155fae62d1b9e5fc7496
lincolneddy <lincoln@essentialcaravans.com>
2025-05-22 14:21:03 +1000

    Essential extentions v18.0.4.0.0 [SKU=new field]

--- esse_extensions/models/product_category.py
+++ esse_extensions/models/product_category.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
     sku_prefix = fields.Char(string="SKU Prefix", size=2)
+
+    @api.depends('sku_prefix')
+    def _compute_display_name(self):
+        for record in self:
+            if record.sku_prefix:
+                record.display_name = "[" + record.sku_prefix + "] " + record.name
+            else:
+                record.display_name = record.name
